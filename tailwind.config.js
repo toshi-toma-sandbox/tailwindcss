84d3ae21e962d4cb3ec0cf5c5ef68f2127a6df51
toshi-toma <toshitoma1217@gmail.com>
2020-10-25 11:35:56 +0900

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+    spacing: {
+      sm: "8px",
+      md: "16px",
+      lg: "24px",
+      xl: "48px",
+    },
+    height: {
+      sm: "8px",
+      md: "16px",
+      lg: "24px",
+      xl: "48px",
+    },
+  },
+
+  variants: {},
+  plugins: [],
+};
